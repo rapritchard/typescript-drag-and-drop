1357c9b86f6ad98aa2b818606c8bdb05da11d3ea
Reece Pritchard <32645551+rapritchard@users.noreply.github.com>
2021-05-11 16:49:05 +0100

    Namespace file organisation.

---
+++ src/decorators/autobind.ts
@@ -0,0 +1,25 @@
+namespace App {
+  /**
+   * Decorator function that automatically handles binding of this
+   * @function
+   * @param {any} target - Either constructor function of the class for a static member, or prototype of the class for an instance member
+   * @param {string} methodName - Name of the member
+   * @param {PropertyDescriptor} descriptor - Property Descriptor for the member (Undefined if script target is < ES5)
+   * @return {PropertyDescriptor} Adjusted descriptor (Return value is ignored if script target is < ES5)
+   */
+  export function autobind(
+    _target: any,
+    _methodName: string,
+    descriptor: PropertyDescriptor
+  ) {
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
---
+++ src/util/validation.ts
@@ -0,0 +1,57 @@
+namespace App {
+  export interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+  }
+
+  /**
+   * Determines if input is valid or not
+   * @function
+   * @param {Object} validatableInput - Input to be validated.
+   * @param {string|number} validatableInput.value - Value of input.
+   * @param {boolean=} validatableInput.required - Determines if value is required
+   * @param {number} validatableInput.minLength - Minimum length of string validatableInput.value
+   * @param {number} validatableInput.maxLength - Maximum length of string validatableInput.value
+   * @param {number} validatableInput.min - Minimum length of number validatableInput.value
+   * @param {number} validatableInput.max - Maximum length of number validatableInput.value
+   * @returns {boolean} True for is valid, false for not valid
+   */
+  export function validate(validatableInput: Validatable) {
+    let isValid = true;
+    if (validatableInput.required) {
+      isValid =
+        isValid && validatableInput.value.toString().trim().length !== 0;
+    }
+    if (
+      validatableInput.minLength != null &&
+      typeof validatableInput.value === "string"
+    ) {
+      isValid =
+        isValid && validatableInput.value.length >= validatableInput.minLength;
+    }
+    if (
+      validatableInput.maxLength != null &&
+      typeof validatableInput.value === "string"
+    ) {
+      isValid =
+        isValid && validatableInput.value.length <= validatableInput.maxLength;
+    }
+    if (
+      validatableInput.min != null &&
+      typeof validatableInput.value === "number"
+    ) {
+      isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (
+      validatableInput.max != null &&
+      typeof validatableInput.value === "number"
+    ) {
+      isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+  }
+}
