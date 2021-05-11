2f3d71cf4e0443a60c1884e8b04f833f11a9f5d4
Reece Pritchard <32645551+rapritchard@users.noreply.github.com>
2021-05-11 17:42:08 +0100

    Showcasing different import / export styles.

--- src/components/base-component.ts
+++ src/components/base-component.ts
@@ -3,7 +3,10 @@
  * @class
  * @abstract
  */
-export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+export default abstract class Component<
+  T extends HTMLElement,
+  U extends HTMLElement
+> {
   templateElement: HTMLTemplateElement;
   hostElement: T;
   element: U;
--- src/components/project-list.ts
+++ src/components/project-list.ts
@@ -1,5 +1,5 @@
 import { DragTarget } from "../models/drag-drop.js";
-import { Component } from "./base-component.js";
+import Component from "./base-component.js";
 import { Project, ProjectStatus } from "../models/project.js";
 import { projectState } from "../state/project.js";
 import { autobind } from "../decorators/autobind.js";
